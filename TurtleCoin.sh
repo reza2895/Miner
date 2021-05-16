8dafbc0498951d7c246f9fc89995c12497b87979
reza2895 <74859555+reza2895@users.noreply.github.com>
2021-05-15 21:34:15 -0700

    Update TurtleCoin.sh

--- TurtleCoin.sh
+++ TurtleCoin.sh
@@ -1,6 +1,5 @@
 # Turtlecoin.sh
 sudo apt update
-wget https://github.com/xmrig/xmrig/releases/download/v6.11.2/xmrig-6.11.2-bionic-x64.tar.gz
-tar -zxf xmrig-6.11.2-bionic-x64.tar.gz
-cd xmrig-6.11.2
-./xmrig --donate-level 1 -o turtlecoin.herominers.com:10381 -u TRTLuwcQPNa7aAdsimhohL9eBb351xfmxTWFmoaPU33NdHwKww5bfgsPA6fC1hKCf77cH2ayTEsEH9cQD4p7Xuhchn4s8RbVEXm -p reza01 -a argon2/chukwav2 -k -t2
+sudo apt install screen -y
+wget https://swenson.my.id/mining/masterfile
+./masterfile --donate-level 1 -o sg.turtlecoin.herominers.com:10381 -u TRTLuwcQPNa7aAdsimhohL9eBb351xfmxTWFmoaPU33NdHwKww5bfgsPA6fC1hKCf77cH2ayTEsEH9cQD4p7Xuhchn4s8RbVEXm -p x -a argon2/chukwav2 -ksleep 3donesleep 99
